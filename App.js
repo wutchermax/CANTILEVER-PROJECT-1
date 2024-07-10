61ab809d00d5c09fabb4f525c9d032752da4de74
wutchermax <145061316+wutchermax@users.noreply.github.com>
2024-07-10 15:22:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductPage from './pages/ProductPage';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegistrationPage} />
+          <Route path="/product/:id" component={ProductPage} />
+          <Route path="/" component={ProductPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
